e6967e06a5ff5d9aa0ae871ada4a585c2383272e
FriedrichM <junkspam47@gmail.com>
2021-02-09 09:58:57 +0200

    added frontend build

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ morgan.token('data', (req)=> {
 
 const app = express()
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server now running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
 
 let persons =  [
