fd62038ab6604a6c51be9616ad79d1871f38ed68
FriedrichM <junkspam47@gmail.com>
2021-02-13 12:11:18 +0200

    done but wrong id

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+const Person = require('./models/person')
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
@@ -13,75 +15,113 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
-let persons =  [
-    {
-      "name": "Arto Hellas",
-      "number": "040-12345",
-      "id": 1
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
-    }
-  ]
 
 app.get('/info', (req, res) => {
-  res.send('Phonebook has info on '+persons.length+' people</br> '+new Date())
+  Person.countDocuments({}, (err, count)=> {
+    if (err) {
+      return response.status(500).json({error: 'db error'})
+    }
+    res.send('Phonebook has info on '+count+' people</br> '+new Date())
+  }).catch(error => next(error));
+
 })
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(result => {
+    res.json(result)
+  }).catch(error => next(error))
+
 })
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
+  if (body.name === undefined||body.number === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
   if (!body.number||!body.name) {
     return response.status(400).json({
       error: 'required field missing'
     })
   }
-  if (persons.find(person => person.name === body.name)) {
-    return response.status(409).json({
-      error: 'name must be unique'
-    })
-  }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
     id:Math.floor(Math.random()*1000000000000)
-  }
-  persons = persons.concat(person)
-  response.json(person)
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  }).catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const body = request.body
+
+  if (body.name === undefined||body.number === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+  if (!body.number||!body.name) {
+    return response.status(400).json({
+      error: 'required field missing'
+    })
+  }
+
+  Person.findOneAndUpdate({id:id},
+    {name:body.name, number:body.number},
+    {returnOriginal: false})
+    .then(savedPerson => {
+      if(savedPerson){
+        response.json(savedPerson)
+      }else{
+        response.status(404).end()
+      }
+
+  }).catch(error => next(error))
+})
+
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if(person) {
-    response.json(person)
-  }else{
-    response.status(404).end()
-  }
+  const person = Person.findOne({id:id}).then(result => {
+    if (result) {
+      response.json(result)
+    } else {
+      response.status(404).end()
+    }
+  }).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id',(request, response) => {
   const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+  Person.deleteOne({ id: id }, ()=> {
+    response.status(204).end()
+  }).catch(error => next(error));
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }else{
+    return response.status(400).send({ error: 'unknown error' })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
